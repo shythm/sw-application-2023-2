584aa1a6ff5341e4b389b5dfdaeba434d50a6d53
Seongho Lee <shythm@outlook.com>
2023-12-08 04:03:39 +0900

    update: hdd-list data and view

--- app/hdd-relver-system/src/App.js
+++ app/hdd-relver-system/src/App.js
@@ -11,125 +11,31 @@ import ReliabilityTable from "./components/ReliabilityTable";
 import FailureRateChart from "./components/FailureRateChart";
 
 export default function App() {
-  const sampleData = [
-    {
-      id: 1,
-      manufacturer: "Manufacturer A",
-      model: "Model A",
-      capacity: "1TB",
-      rpm: "7200",
-      price: "$100",
-      errorRate: "0.2%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 2,
-      manufacturer: "Manufacturer B",
-      model: "Model B",
-      capacity: "2TB",
-      rpm: "5400",
-      price: "$200",
-      errorRate: "0.1%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 3,
-      manufacturer: "Manufacturer C",
-      model: "Model C",
-      capacity: "500GB",
-      rpm: "7200",
-      price: "$150",
-      errorRate: "0.3%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 4,
-      manufacturer: "Manufacturer D",
-      model: "Model D",
-      capacity: "1TB",
-      rpm: "5400",
-      price: "$120",
-      errorRate: "0.4%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 5,
-      manufacturer: "Manufacturer E",
-      model: "Model E",
-      capacity: "2TB",
-      rpm: "7200",
-      price: "$220",
-      errorRate: "0.5%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 6,
-      manufacturer: "Manufacturer F",
-      model: "Model F",
-      capacity: "500GB",
-      rpm: "5400",
-      price: "$110",
-      errorRate: "0.6%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 7,
-      manufacturer: "Manufacturer G",
-      model: "Model G",
-      capacity: "1TB",
-      rpm: "7200",
-      price: "$130",
-      errorRate: "0.7%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 8,
-      manufacturer: "Manufacturer H",
-      model: "Model H",
-      capacity: "2TB",
-      rpm: "5400",
-      price: "$210",
-      errorRate: "0.8%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 9,
-      manufacturer: "Manufacturer I",
-      model: "Model I",
-      capacity: "500GB",
-      rpm: "7200",
-      price: "$140",
-      errorRate: "0.9%",
-      purchaseLink: "http://example.com",
-    },
-    {
-      id: 10,
-      manufacturer: "Manufacturer J",
-      model: "Model J",
-      capacity: "1TB",
-      rpm: "5400",
-      price: "$120",
-      errorRate: "1.0%",
-      purchaseLink: "http://example.com",
-    },
-  ];
-
+  const [hardDiskData, setHardDiskData] = useState(null);
   const [data, setData] = useState(null);
+
+  if (!hardDiskData) {
+    Papa.parse("/data/hdd-list-2022.csv", {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+      complete: (res) => setHardDiskData(res.data),
+    });
+  }
+  console.log(hardDiskData);
 
   if (!data) {
     Papa.parse("/data/ST14000NM001G-smart_9_raw.csv", {
       download: true,
-      header: false,
       complete: function (results) {
         setData(results.data.slice(1));
       },
     });
   }
 
-  console.log(data);
-
   return (
-    <Container fluid="lg" className="p-4">
+    <Container fluid className="p-4">
       <Row className="mt-4">
         <Col sm={6} md={3}>
           <Image
@@ -142,23 +48,23 @@ export default function App() {
       </Row>
       <Row className="mt-2">
         <Col>
-          <h1 className="display-4 fw-bold">
+          <h1>
             <strong>
-              실사용 데이터 기반
+              <span className="fw-bold">실사용 데이터 기반</span>
               <br />
-              HDD 신뢰도 검증 시스템
+              <span className="display-4 fw-bold">HDD 신뢰도 검증 시스템</span>
             </strong>
           </h1>
         </Col>
       </Row>
       <Row className="mt-2">
-        <Col sm={12} md={7}>
+        <Col sm={12} md={8} className="overflow-scroll">
           <ReliabilityTable
-            data={sampleData}
+            data={hardDiskData}
             onRowClick={(e) => console.log(e)}
           />
         </Col>
-        <Col sm={12} md={5}>
+        <Col sm={12} md={4}>
           <FailureRateChart
             dataX={data?.map((row) => row[0])}
             dataY={data?.map((row) => row[1] * 100)}
--- app/hdd-relver-system/src/components/ReliabilityTable.jsx
+++ app/hdd-relver-system/src/components/ReliabilityTable.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 
-const ReliabilityTable = ({ data, onRowClick }) => {
+const ReliabilityTable = (props) => {
+  const { data, onRowClick } = props;
+
+  if (!data) {
+    return <div>데이터가 없습니다.</div>;
+  }
+
+  // sort by errorRate
+  let sortedData = data.slice();
+  sortedData.sort((a, b) => a.errorRate - b.errorRate);
+
   return (
-    <Table striped bordered hover>
+    <Table striped hover bordered>
       <thead>
         <tr>
           <th>번호</th>
-          <th>제조 회사</th>
+          <th>제조회사</th>
           <th>모델명</th>
-          <th>용량</th>
+          <th>용량(TB)</th>
           <th>RPM</th>
-          <th>가격</th>
+          <th>가격(₩)</th>
           <th>에러율</th>
-          <th>구매 링크</th>
+          <th>구매</th>
         </tr>
       </thead>
       <tbody>
-        {data.map((item) => (
+        {sortedData.map((item, idx) => (
           <tr key={item.id} onClick={() => onRowClick(item)}>
-            <td>{item.id}</td>
+            <td>{idx + 1}</td>
             <td>{item.manufacturer}</td>
             <td>{item.model}</td>
             <td>{item.capacity}</td>
             <td>{item.rpm}</td>
-            <td>{item.price}</td>
-            <td>{item.errorRate}</td>
+            <td>{item.price?.toLocaleString()}</td>
+            <td>{item.errorRate?.toFixed(8)}</td>
             <td>
               <a href={item.purchaseLink}>구매</a>
             </td>
